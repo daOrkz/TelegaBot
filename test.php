d7cba9710acfb073c8428030e91f2505261d6466
daOrkz <fillippok87@gmail.com>
2023-12-30 22:30:22 +0700

    test Weather complite

--- test.php
+++ test.php
@@ -1,87 +1,142 @@
 <?php
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
- */
+ini_set('error_reporting', E_ALL);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
 
 require_once 'src/init.php';
-interface iStrategyCommand
+
+use Bot\Util\Logger as Logger;
+
+$ErrLogger = new Logger('/Logs', '/errLogs.txt');
+$logger = new Logger();
+
+class Weather
 {
-    public function execute($command);
+    static string $currentWeatherUrl = 'https://weatherapi-com.p.rapidapi.com/current.json?q=53.45154224%2C87.38461796';
+    static string $forecasttWeatherUrl = 'https://weatherapi-com.p.rapidapi.com/forecast.json?q=53.45154224%2C87.38461796&days=3';
+    
+    static array $curlOpt = [
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => "",
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 30,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => "GET",
+        CURLOPT_HTTPHEADER => [
+            "X-RapidAPI-Host: weatherapi-com.p.rapidapi.com",
+            "X-RapidAPI-Key: 54e1575496msh4b881834862ec00p1160f5jsn5e68d631b210"
+        ],
+    ];
+    static array $moonPhase = [
+        'New Moon' => 'Новолуние',
+        'Waxing Crescent' => 'Растущая луна',
+        'First Quarter' => 'Первая четверть луны',
+        'Waxing Gibbous' => 'Почти полная, возррастающая луна',
+        'Full Moon' => 'Полнлуние',
+        'Waning Gibbous' => 'Почти полная, убывающая луна',
+        'Last Quarter' => 'Последняя четверть луны',
+        'Waning Crescen' => 'Месяц',
+    ];
+
+    static function getCurrentWeather(): string
+    {
+
+        $curl = curl_init(self::$currentWeatherUrl);
+
+        curl_setopt_array($curl, self::$curlOpt);
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
+
+        if (curl_errno($curl)) {
+            throw new CURLException(curl_error($curl));
+        }
+
+        $currentWeather = json_decode($response, true);
+        
+        $weathetTextMessage = "Температура в Мысках: <b>{$currentWeather['current']['temp_c']}</b>" . PHP_EOL
+           . "Ощущается как: {$currentWeather['current']['feelslike_c']}" . PHP_EOL
+           . "Скорость ветра: {$currentWeather['current']['wind_kph']}";
+           
+       return $weathetTextMessage;
+    }
+
+    static function getForecastWeather(): string
+    {
+
+        $curl = curl_init(self::$forecasttWeatherUrl);
+
+
+        curl_setopt_array($curl, self::$curlOpt);
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
+
+        if (curl_errno($curl)) {
+            throw new CURLException(curl_error($curl));
+        }
+
+        $weather = json_decode($response, true);
+        
+        $forecastDays = $weather['forecast']['forecastday'];
+
+        $weathetTextMessage = '';
+        
+        foreach ($forecastDays as $forecastDay) {
+
+            $weathetTextMessage .= " -- <b>{$forecastDay['date']}</b> -- <br>"
+            . "Температура от {$forecastDay['day']['maxtemp_c']} до {$forecastDay['day']['mintemp_c']} <br>"
+            . "Вероятность снега: {$forecastDay['day']['daily_chance_of_snow']}% <br>"
+            . "Вероятность дождя: {$forecastDay['day']['daily_chance_of_rain']}% <br>"
+            . "Восход: {$forecastDay['astro']['sunrise']} <br>"
+            . "Закат: {$forecastDay['astro']['sunset']} <br>"
+            . "Фаза луны: " . self::$moonPhase[$forecastDay['astro']['moon_phase']] . "<br>"
+            . "Видимость луны: {$forecastDay['astro']['moon_illumination']}% <br>"
+            . '<br>';
+        }
+        
+        return $weathetTextMessage;
+    }
+
 }
 
-class StartCommand implements iStrategyCommand
-{
-    public function execute($command)
-    {
-        return 'start';
-    }
 
-}
-
-class TimeCommand implements iStrategyCommand
-{
-    public function execute($command)
-    {
-        return 'time';
-    }
-
-}
-
-class WeatherCommand implements iStrategyCommand
-{
-    public function execute($command)
-    {
-        return 'weather';
-    }
-
-}
-
-class ContextCommand
-{
-    private iStrategyCommand $strategy;
-
-    public function setStrategy(iStrategyCommand $strategy)
-    {
-        $this->strategy = $strategy;
-    }
-
-    public function executeStrategy($command)
-    {
-        return $this->strategy->execute($command);
-    }
-
-}
-
-$commands = ['/time', '/weather', '/start'];
-
-$context = new ContextCommand();
-
-foreach ($commands as $command) {
+echo Weather::getForecastWeather();
 
 
+exit();
 
-    switch ($command) {
-        case '/start':
-            $context->setStrategy(new StartCommand());
-            echo $context->executeStrategy($command);
+$weather = Weather::getForecastWeather();
+$forecastDays = $weather['forecast']['forecastday'];
 
-            break;
+$moonPhase = [
+    'New Moon' => 'Новолуние',
+    'Waxing Crescent' => 'Растущая луна',
+    'First Quarter' => 'Первая четверть луны',
+    'Waxing Gibbous' => 'Почти полная, возррастающая луна',
+    'Full Moon' => 'Полнлуние',
+    'Waning Gibbous' => 'Почти полная, убывающая луна',
+    'Last Quarter' => 'Последняя четверть луны',
+    'Waning Crescen' => 'Месяц',
+];
 
-        case '/time':
-            $context->setStrategy(new TimeCommand());
-            echo $context->executeStrategy($command);
-
-            break;
-
-        case '/weather':
-            $context->setStrategy(new WeatherCommand());
-            echo $context->executeStrategy($command);
-
-            break;
-
-        default:
-            break;
-    }
+foreach ($forecastDays as $forecastDay) {
+    echo '<pre>';
+//    print_r($forecastDay);
+//    print_r($forecastDay['date']);
+//    print_r($forecastDay['day']);
+//    print_r($forecastDay['astro']);
+    echo '</pre>';
+    echo "{$forecastDay['date']} <br>"
+    . "Температура от {$forecastDay['day']['maxtemp_c']} до {$forecastDay['day']['mintemp_c']} <br>"
+    . "Вероятность снега: {$forecastDay['day']['daily_chance_of_snow']}% <br>"
+    . "Вероятность дождя: {$forecastDay['day']['daily_chance_of_rain']}% <br>"
+    . "Восход: {$forecastDay['astro']['sunrise']} <br>"
+    . "Закат: {$forecastDay['astro']['sunset']} <br>"
+    . "Фаза луны: " . $moonPhase[$forecastDay['astro']['moon_phase']] . '<br>'
+    . "Видимость луны: {$forecastDay['astro']['moon_illumination']} <br>"
+    ;
 }
