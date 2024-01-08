d7bea346023f179675f8b1df0de5a3d7c1258386
daOrkz <fillippok87@gmail.com>
2024-01-08 19:21:11 +0700

    delete old telega bot

--- index.php
+++ index.php
@@ -1,9 +1,5 @@
 <?php
 
-ini_set('error_reporting', E_ALL);
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-
 require_once 'src/init.php';
 
 use Bot\Exceptions\{
@@ -15,16 +11,7 @@ use Bot\Util\{
     Logger,
     InputUser
 };
-//use Bot\TelegramBot\CurlPost\CurlPostFieldBuilder\{
-//    CurlPostFieldHtmlBuilder,
-//    CurlPostFieldAdminBuilder,
-//    CurlPostFieldMdBuilder
-//};
-//use Bot\TelegramBot\CommandStrategy\ContextCommand;
 
-//use Bot\TelegramBot\TelegramBot;
-
-//use Bot\Facades\Commands\{Start, Time, Weather, Forecast, UnknownCommand};
 use Bot\Facades\{Commands, Exception};
 
 $config = parse_ini_file('config.ini');
@@ -36,11 +23,7 @@ $messageText = InputUser::getInput($data);
 $ErrLogger = new Logger('/Logs', '/errLogs.txt');
 $logger = new Logger();
 
-//$logger->writeLog($messageText);
-//$logger->writeLog($messageText, true);
-//$logger->writeLog($data->callback_query->from->id);
 
-//$messageText = '/time';
 try {
     switch ($messageText) {
         case '/start':
